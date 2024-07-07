edbf58fd072569bcb9b5aaf05343239263d637ea
sorry0not1kot2 <162019473+sorry0not1kot2@users.noreply.github.com>
2024-07-07 20:09:50 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 # файл main.py
 import os
 import asyncio
+import logging
 from telebot.async_telebot import AsyncTeleBot
 import handlers
+import translations  # Импортируем translations
+
+# Настройка логирования
+logging.basicConfig(level=logging.INFO)
 
 # Настройка бота
 BOT_TOKEN = os.getenv('TELEGRAM_BOT_TOKEN')
@@ -10,7 +15,8 @@ bot = AsyncTeleBot(BOT_TOKEN)
 
 # Обработчик команды /start
 async def start(message):
-    await bot.send_message(message.chat.id, "Привет! Я ботGPT-4. Слушаю тебя.")
+    приветствие = translations['start']  # Используем переменную из translations
+    await bot.send_message(message.chat.id, f"{приветствие} Слушаю тебя.")
 
 # Добавление обработчиков
 bot.register_message_handler(start, commands=['start'])
@@ -18,6 +24,7 @@ bot.register_message_handler(lambda message: handlers.handle_text(bot, message),
 
 # Асинхронная функция main для запуска бота
 async def main():
+    logging.info("Бот запущен")
     await bot.polling(none_stop=True, timeout=60)
 
 # Запуск бота
